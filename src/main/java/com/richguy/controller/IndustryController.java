b8b2631b2e4d5838ca9f8e84103eb8b4bce0948f
jaysunxiao <jaysunxiao.gmail.com>
2022-01-15 00:19:40 +0800

    doc

--- src/main/java/com/richguy/controller/IndustryController.java
+++ src/main/java/com/richguy/controller/IndustryController.java
@@ -1,15 +1,25 @@
 package com.richguy.controller;
 
+import com.richguy.resource.IndustryResource;
 import com.richguy.service.IndustryService;
 import com.richguy.service.RichGuyService;
 import com.richguy.service.TopWordService;
+import com.richguy.util.IndustryUtils;
+import com.zfoo.protocol.collection.CollectionUtils;
+import com.zfoo.protocol.model.Pair;
+import com.zfoo.protocol.util.FileUtils;
+import com.zfoo.protocol.util.StringUtils;
 import com.zfoo.scheduler.model.anno.Scheduler;
+import com.zfoo.storage.model.anno.ResInjection;
+import com.zfoo.storage.model.vo.Storage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -31,6 +41,9 @@ public class IndustryController {
     @Value("${qq.pushGroupIds}")
     private List<Long> pushGroupIds;
 
+    @ResInjection
+    private Storage<Integer, IndustryResource> industryResources;
+
     @Scheduler(cron = "30 1 0 * * ?")
     public void cronPushTop() {
         var topIndustry = industryService.topIndustryToday();
@@ -46,6 +59,46 @@ public class IndustryController {
         for (var pushGroupId : pushGroupIds) {
             var group = bot.getGroup(pushGroupId);
             group.sendMessage(topWord);
+        }
+    }
+
+    @Scheduler(cron = "0 0/10 * * * ?")
+    public void cronNewIndustry() throws IOException, InterruptedException {
+        var allIndustry = IndustryUtils.allIndustryList();
+        var newIndustrySet = new HashSet<Pair<Integer, String>>();
+        for (var industry : allIndustry) {
+            var flag = industryResources.getAll()
+                    .stream()
+                    .noneMatch(it -> it.getName().equals(StringUtils.trim(industry.getValue())));
+            if (flag) {
+                newIndustrySet.add(industry);
+            }
+
+            if (!industryResources.contain(industry.getKey())) {
+                newIndustrySet.add(industry);
+            }
+        }
+
+        if (CollectionUtils.isEmpty(newIndustrySet)) {
+            return;
+        }
+
+        var builder = new StringBuilder();
+        builder.append("\uD83D\uDCA5紧急通知新概念出现：");
+        builder.append(FileUtils.LS);
+        builder.append(FileUtils.LS);
+        for (var industry : newIndustrySet) {
+            builder.append(StringUtils.format("{} {} ", industry.getKey(), industry.getValue()));
+            builder.append(FileUtils.LS);
+        }
+
+        var bot = richGuyService.bot;
+
+        var newIndustryContent = builder.toString();
+
+        for (var pushGroupId : pushGroupIds) {
+            var group = bot.getGroup(pushGroupId);
+            group.sendMessage(newIndustryContent);
         }
     }
 
